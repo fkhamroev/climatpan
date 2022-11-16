d25e1ca8bcb06546c8aa375b32ed675977205200
firuz <you@example.com>
2022-11-16 20:07:02 +0500

    second

---
+++ sandmail.php
@@ -0,0 +1,15 @@
+<? 
+    $to = 'edjaxarov@mail.ru';
+    $from = ($_POST['user__phone']);
+
+    $headers = "From: $from". "\r\n". 
+    "Reply-to: $from".  "\r\n".
+    "X-Mailer: PHP/ " . phpversion();
+
+
+    if (mail($to, $headers)) {
+        echo "Письмо отправлена";
+    }
+    else {
+        echo "Письмо не отправлена";
+    }
